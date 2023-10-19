1e53e019a481fc0ef5d58a12c9f40196482c0a62
Huddy2022 <mlvnhud@aol.com>
2023-10-19 11:33:44 +0000

    add intial set up with index.html, add folders in assets for css, images & js with a style.css, a main.js for kaboom and also a public folder for the scene background images, sounds and sprites

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+// initialize kaboom context
+kaboom({
+    width: 800,
+    height: 600,
+    font: "sans-serif",
+    canvas: document.getElementById("game-canvas"),
+    background: [ 0, 0, 255, ],
+})
