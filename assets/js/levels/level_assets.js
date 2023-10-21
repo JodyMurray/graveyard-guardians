4c54c7168147573f897ac074a4fd7f3c7f6e675d
BogdanCatalin-Iacob <bogdancatalin.iacob@gmail.com>
2023-10-21 10:24:59 +0100

    Load map assets (tiles)

---
+++ assets/js/levels/level_assets.js
@@ -0,0 +1,133 @@
+/**
+ * Load level assets (tiles and objects)
+ */
+export default function loadLevelAssets() {
+  loadSprite(
+    'top-left-corner',
+    "public/sprites/Tiles/Tile (1).png",
+    {
+      sliceX: 1,
+      sliceY: 1
+    }
+  );
+  loadSprite(
+    'top-middle',
+    "public/sprites/Tiles/Tile (2).png",
+    {
+      sliceX: 1,
+      sliceY: 1
+    }
+  );
+  loadSprite(
+    'top-right-corner',
+    "public/sprites/Tiles/Tile (3).png",
+    {
+      sliceX: 1,
+      sliceY: 1
+    }
+  );
+  loadSprite(
+    'middle-left',
+    "public/sprites/Tiles/Tile (4).png",
+    {
+      sliceX: 1,
+      sliceY: 1
+    }
+  );
+  loadSprite(
+    'middle-middle',
+    "public/sprites/Tiles/Tile (5).png",
+    {
+      sliceX: 1,
+      sliceY: 1
+    }
+  );
+  loadSprite(
+    'middle-right',
+    "public/sprites/Tiles/Tile (6).png",
+    {
+      sliceX: 1,
+      sliceY: 1
+    }
+  );
+  loadSprite(
+    'top-right',
+    "public/sprites/Tiles/Tile (7).png",
+    {
+      sliceX: 1,
+      sliceY: 1
+    }
+  );
+  loadSprite(
+    'top-right-end',
+    "public/sprites/Tiles/Tile (8).png",
+    {
+      sliceX: 1,
+      sliceY: 1
+    }
+  );
+  loadSprite(
+    'bottom-middle',
+    "public/sprites/Tiles/Tile (9).png",
+    {
+      sliceX: 1,
+      sliceY: 1
+    }
+  );
+  loadSprite(
+    'top-left-end',
+    "public/sprites/Tiles/Tile (10).png",
+    {
+      sliceX: 1,
+      sliceY: 1
+    }
+  );
+  loadSprite(
+    'top-left',
+    "public/sprites/Tiles/Tile (11).png",
+    {
+      sliceX: 1,
+      sliceY: 1
+    }
+  );
+  loadSprite(
+    'bottom-left',
+    "public/sprites/Tiles/Tile (12).png",
+    {
+      sliceX: 1,
+      sliceY: 1
+    }
+  );
+  loadSprite(
+    'bottom-right',
+    "public/sprites/Tiles/Tile (13).png",
+    {
+      sliceX: 1,
+      sliceY: 1
+    }
+  );
+  loadSprite(
+    'float-left-corner',
+    "public/sprites/Tiles/Tile (14).png",
+    {
+      sliceX: 1,
+      sliceY: 1
+    }
+  );
+  loadSprite(
+    'float-middle',
+    "public/sprites/Tiles/Tile (15).png",
+    {
+      sliceX: 1,
+      sliceY: 1
+    }
+  );
+  loadSprite(
+    'float-right',
+    "public/sprites/Tiles/Tile (16).png",
+    {
+      sliceX: 1,
+      sliceY: 1
+    }
+  );
+}
